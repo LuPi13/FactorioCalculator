4237bec544ff0dd1f50a756b242b2231ee105b16
LuPi13 <lupi1313@naver.com>
2024-10-29 16:13:15 +0900

    Make recursive assember counter

--- main.py
+++ main.py
@@ -3,9 +3,11 @@
 """
 import csv
 
-# 전역 변수; 조립기계2, 강철 용광로 계수
+# 전역 변수
 assembler_coef = 0.75
 furnace_coef = 2
+terminal_ingredients = ["철 판", "구리 판", "돌", "석탄"]
+denying = ["", "철 광석", "구리 광석", "돌", "석탄"]
 
 
 
@@ -16,21 +18,32 @@ def find_row(name, file_path="data.csv"):
         next(reader)
         for row in reader:
             if row[0] == name:
+                file.close
                 return row
-    raise Exception("해당 이름을 가진 row가 없습니다.")
+    raise Exception(f"{name}이라는 이름을 가진 row가 없습니다.")
 
 
-# row를 받아서, 해당 재료 '하나' 만드는 데에 필요한 철 판과 구리 판 개수를 튜플 (철, 구리)로 변환하는 함수
-def convert_to_iron_and_copper(row):
+# row를 받아서, 해당 재료 '하나' 만드는 데에 필요한 말단 재료로 변환하는 함수
+def convert_to_terminal_ingredients(row):
     divider = float(row[2])
-    iron = 0
-    copper = 0
+    count = [0 for _ in range(len(terminal_ingredients))]
     for i in range(4, 16, 2):
+        for j in range(0, len(terminal_ingredients)):
+            if (row[i] == terminal_ingredients[j]):
+                count[j] += float(row[i+1])
+            if (row[i] in denying):
+                continue
+            else:
+                count[j] += convert_to_terminal_ingredients(find_row(row[i]))[j] * float(row[i+1])
+    for k in range(len(terminal_ingredients)):
+        count[k] /= divider
+    return count
+"""
         if row[i] == "철 판":
             iron += float(row[i+1])
         elif row[i] == "구리 판":
             copper += float(row[i+1])
-        elif ((row[i] == "구리 광석") or (row[i] == "철 광석") or (row[i] == "")):
+        elif ((row[i] in terminal_ingredient) or (row[i] == "")):
             continue
         else: # 재귀적으로 호출
             iron += convert_to_iron_and_copper(find_row(row[i]))[0] * float(row[i+1])
@@ -39,41 +52,67 @@ def convert_to_iron_and_copper(row):
     iron /= divider
     copper /= divider
     return iron, copper
+"""
 
 
-# 목표 아이템을 1초에 count개 만들 때, 필요한 조립기계 개수
+# 목표 아이템을 1초에 count개 만들 때, 필요한 제작기와 개수
 def assembler_per_count(item, count):
     row = find_row(item)
-    return (count * float(row[3])) / (float(row[2]) * assembler_coef)
+    count
+    if row[1] == "조립기계":
+        count = (count * float(row[3])) / (float(row[2]) * assembler_coef)
+    elif row[1] == "용광로":
+        count = (count * float(row[3])) / (float(row[2]) * furnace_coef)
+    return row[1], count
 
 
-# 재귀적으로 필요한 조립기계 개수 print
-def print_assembler_recursive(item, count):
+# 필요한 제작기 개수 print
+def print_assembler(item, count):
     row = find_row(item)
-    assembler = assembler_per_count(item, count)
-    print(f"1초에 {item} {count}개 만들 때 필요한 조립기계 개수: {round(assembler, 3)}개 (필요 재료: ", end="")
+    crafter, assembler_count = assembler_per_count(item, count)
+    print(f"1초에 {item} {count}개 만들 때 필요한 {crafter} 개수: {round(assembler_count, 3)}개 (필요 재료: ", end="")
     for i in range(4, 16, 2):
         if (row[i] != ""):
-            print(f"{row[i]} {round(count * float(row[i+1]), 3)}개, ", end="")
+            if ((i != 4) and (row[i] != "")):
+                print(", ", end="")
+            print(f"{row[i]} {round(count * float(row[i+1]), 3)}개", end="")
+    print(")")
+
+
+# 재귀적으로 필요한 제작기 개수 print
+def print_assembler_recursive(item, count):
+    row = find_row(item)
+    crafter, assembler_count = assembler_per_count(item, count)
+    print(f"1초에 {item} {count}개 만들 때 필요한 {crafter} 개수: {round(assembler_count, 3)}개 (필요 재료: ", end="")
+    for i in range(4, 16, 2):
+        if (row[i] != ""):
+            if ((i != 4) and (row[i] != "")):
+                print(", ", end="")
+            print(f"{row[i]} {round(count * float(row[i+1]), 3)}개", end="")
     print(")")
     for i in range(4, 16, 2):
-        if not((row[i] == "구리 판") or (row[i] == "철 판") or (row[i] == "")):
+        if not((row[i] in terminal_ingredients) or (row[i] == "")):
             print_assembler_recursive(row[i], count * float(row[i+1]))
 
 
 # 한번에 다 출력하는 함수
 def print_all_info(item, count):
     row = find_row(item)
-    iron, copper = convert_to_iron_and_copper(row)
+    terminal = convert_to_terminal_ingredients(row)
     print(row)
-    print(f"필요한 철: {iron}, 필요한 구리: {copper}")
+    for element in terminal:
+        print(f"{terminal_ingredients[terminal.index(element)]}: {element}개", end="")
+        if (terminal.index(element) != len(terminal) - 1):
+            print(", ", end="")
+    print("")
     print_assembler_recursive(item, count)
+    print("")
 
 
 def main():
-    print_all_info("전자 회로", 3)
     print_all_info("자동화 과학 팩", 0.75)
     print_all_info("물류 과학 팩", 0.75)
+    print_all_info("군사 과학 팩", 0.75)
 
 
 
